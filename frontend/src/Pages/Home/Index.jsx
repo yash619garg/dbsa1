9aa574e04f847a286e15f2767dc2c31b79230911
yash garg <yash_2022bcse056@nitsri.ac.in>
2024-05-11 13:42:31 +0530

    scrollbar 1

--- frontend/src/Pages/Home/Index.jsx
+++ frontend/src/Pages/Home/Index.jsx
@@ -7,7 +7,6 @@ import TopProducts from "./TopProducts";
 import Banner from "./Banner";
 import Category from "./Category";
 import { RiWhatsappFill } from "react-icons/ri";
-
 import Certificates from "./Certificates";
 import Brands from "./Brands";
 
@@ -18,7 +17,7 @@ const Home = () => {
         href="http://wa.me/+919416482163"
         target="_blank"
         rel="noopener noreferrer"
-        className="fixed top-[120px] sm:top-[100px] z-50 right-[30px]"
+        className="fixed bottom-[120px] sm:top-[100px] z-50 right-[30px]"
       >
         <RiWhatsappFill className="text-[50px] text-green-500 bg-white rounded-full px-2 shadow-md shadow-slate-400" />
       </a>
